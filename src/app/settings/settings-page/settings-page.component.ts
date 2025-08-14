09de4acb06fab13d741e3244c9ff8df26e16f5fe
balota-natan-utcn <balotann61@gmail.com>
2025-08-14 13:47:47 +0200

    added logout button

--- src/app/settings/settings-page/settings-page.component.ts
+++ src/app/settings/settings-page/settings-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings-page',
@@ -7,6 +8,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.css'
 })
-export class SettingsPageComponent {
+export class SettingsPageComponent
+{
+  constructor(private router: Router) {}
 
+  logout()
+  {
+    //remove JWT from local storage
+    localStorage.removeItem('token');
+
+    //go back to login page
+    this.router.navigate(['/login']);
+  }
 }
